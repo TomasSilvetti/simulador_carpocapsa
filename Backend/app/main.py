0ce3567d36fc8fbd11b3d1eb3ae2865830333bf6
luciano <luc02@live.com>
2025-06-09 22:00:52 -0300

    agrega que las trampas puedanir vacias

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from core.simulation import simulation
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -21,8 +22,12 @@ async def read_root():
     return {"message": "Bienvenido a la aplicación FastAPI"}
 
 @app.post("/reportes")
-def generate_simulation(hectares: float, tcs_per_hectare: float, insecticide_per_hectare: float, labor_cost_per_hectare: float, monitoring_cost_per_hectare: float, tramps: int, insecticide_eggs: bool | None, prev_larvaes: int | None):
-    data = simulation(hectares, tcs_per_hectare, insecticide_per_hectare, labor_cost_per_hectare, monitoring_cost_per_hectare, tramps, insecticide_eggs, prev_larvaes)
+def generate_simulation(hectares: float, tcs_per_hectare: float, insecticide_per_hectare: float, labor_cost_per_hectare: float, monitoring_cost_per_hectare: float, tramps:Optional[str] = Query(default=None), insecticide_eggs: bool | None, prev_larvaes:Optional[str] = Query(default=None)):
+    if isinstance(prev_larvaes, str):
+        prev_larvaes = 500 * hectares
+    if isinstance(tramps, str):
+        tramps = None
+    data = simulation(hectares, tcs_per_hectare, insecticide_per_hectare, labor_cost_per_hectare, monitoring_cost_per_hectare, prev_larvaes, tramps, insecticide_eggs)
     print(f"Datos enviados: {data}")
     return data
 
